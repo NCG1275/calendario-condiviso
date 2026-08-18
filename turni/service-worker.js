f09376af9dbbe3ae4599b6dde6319234d313cfcc
NCG1275 <152069408+NCG1275@users.noreply.github.com>
2026-08-18 19:27:11 +0200

    Release 180826.5 valorizza intestazione e mese

--- turni/service-worker.js
+++ turni/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'turni-personali-180826.4';
+const CACHE_NAME = 'turni-personali-180826.5';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=180826.4',
-  './app.js?v=180826.4',
+  './styles.css?v=180826.5',
+  './app.js?v=180826.5',
   './manifest.webmanifest',
   './icon-192-v2.png',
   './icon-512-v2.png',
